f1b36302c4329c920619a7b50e4e8dd3d475c41a
Pavitra Rastogi <rstg.pavitra.11@gmail.com>
2026-06-16 20:44:14 +0530

    code commit4

--- LinkedList/src/intersectionques.java
+++ LinkedList/src/intersectionques.java
@@ -1,6 +1,30 @@
-public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int n =
+import java.util.LinkedList;
+
+public class intersectionques {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        node ten = new node(10);
+        node twenty = new node(20);
+        node thirty = new node(30);
+        node forty = new node(40);
+        node head = ten;
+        ten.setNext(twenty);
+        twenty.setData(21);
+        twenty.setNext(thirty);
+        thirty.setNext(forty);
+        forty.setNext(null);
+        LinkedList<Integer> lli = new LinkedList<>();
+        node ten1 = new node(10);
+        node forty4 = new node(40);
+        node twenty2 = new node(20);
+        node thirty3 = new node(30);
+
+        node head1 = ten1;
+        ten.setNext(forty4);
+        forty4.setNext(twenty2);
+        twenty2.setNext(thirty3);
+        thirty3.setNext(null);
+        main(ListN fp=head);
 
     }
-}
+    }
