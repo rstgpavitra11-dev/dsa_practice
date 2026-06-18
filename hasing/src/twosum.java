134957c4943d015fd599a019bce9511bf96d975e
Pavitra Rastogi <rstg.pavitra.11@gmail.com>
2026-06-18 23:39:31 +0530

    code commit

--- hasing/src/twosum.java
+++ hasing/src/twosum.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+
 public class twosum {
+    public static void main(String[] args) {
+        int nums[]={2,3,8,4,5,6};
+        int target = 7;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i =0; i< nums.length; i++){
+            int tofind= target-nums[i];
+            if(map.containsKey(tofind)){
+                System.out.println(new int[]=map.get(tofind),i);
+            }
+        }
+    }
 }
