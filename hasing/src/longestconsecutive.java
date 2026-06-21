08054c2ec613c569cd003adf9e630806429f98e4
Pavitra Rastogi <rstg.pavitra.11@gmail.com>
2026-06-21 23:59:53 +0530

    code commit

--- hasing/src/longestconsecutive.java
+++ hasing/src/longestconsecutive.java
@@ -2,20 +2,18 @@ import java.util.Arrays;
 
 public class longestconsecutive {
     public static void main(String[] args) {
-        int nums[] = {0,3,7,2,5,8,4,6,0,1};
+        int nums[] = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
         Arrays.sort(nums);
         int current = nums[0];
         int highest = 1;
-        for(int i : nums){
+        for (int i : nums) {
 
-            if(nums[i]==current){
+            if (nums[i] == current) {
 
-            }
-            else if(nums[i]== ++current){
+            } else if (nums[i] == ++current) {
                 highest++;
-            }
-            else {
-                highest=0;
+            } else {
+                highest = 0;
             }
         }
         System.out.println(highest);
